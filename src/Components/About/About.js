7dc57cb1a604362d9f746f44d57334bba0483a1e
jagrunewald <je_grunewald@hotmail.com>
2022-09-04 19:11:03 -0300

    Adjustments to the About page and AllProjects page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,45 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './About.css';
-import option0 from '../../img/op0.jpg';
-import option1 from '../../img/op1.jpeg';
-import option2 from '../../img/op2.jpeg';
-import option3 from '../../img/op3.jpeg';
-import option4 from '../../img/op4.jpeg';
+import icon from '../../img/devblue.png';
 import cv from '../../files/cv.pdf';
 
-const GALLERY = [
-  option0,
-  option1,
-  option2,
-  option3,
-  option4,
-];
-
 export default function About () {
-  const [photoInitial, setPhotoInitial] = useState(option1);
-
-  function changePhoto (index) {
-    setPhotoInitial(GALLERY[index]);
-  }
-
   return (
     <div className='about' id='about'>
       <div className='about-gallery'>
-        <img src={photoInitial} alt='profile' className='gallery-image' />
-        {/* <Gallery onClick={changePhoto} /> */}
-        {/* <div className='gallery-options'>
-          <ul>
-            {GALLERY.map((option, index) => {
-                return (
-                  <li key={index}>
-                    <button onClick={() => changePhoto(index)}>
-                      <img src={option} alt='photo' className='img-options' />
-                    </button>
-                  </li>
-                );
-              })}
-          </ul>				
-        </div>        */}
+        <img src={icon} alt='profile' className='gallery-image' />        
       </div>
       <div className='about-info'>
         <h2>WHO I AM</h2>
